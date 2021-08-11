a38f528f33cd7fc4938498d0cd61b7f1f1be4785
stardomsolutions <47492936+stardomsolutions@users.noreply.github.com>
2021-08-11 07:26:43 +0530

    Update Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -10,7 +10,7 @@ public class Hello extends HttpServlet {
 
    public void init() throws ServletException {
       // Do required initialization
-      message = "Welcome to KSR Consultancies";
+      message = "Welcome to StartdomSolutions";
    }
 
    public void doGet(HttpServletRequest request, HttpServletResponse response)
